1ba53cbbf3dbba8c1498051b90ebf3c1f829095c
Chengnian Sun <chengniansun@gmail.com>
2015-03-02 16:58:44 -0800

    add cherokee

---
+++ cherokee/build/constants.h
@@ -0,0 +1,23 @@
+#ifndef CHEROKEE_COMPILATION_CONSTANTS_H
+#define CHEROKEE_COMPILATION_CONSTANTS_H
+
+# define CHEROKEE_COMPILATION
+# define PCRE_STATIC
+
+# define CHEROKEE_PLUGINDIR   "/usr/local/lib/cherokee"
+# define CHEROKEE_DEPSDIR     "/usr/local/share/cherokee/deps"
+# define CHEROKEE_DATADIR     "/usr/local/share/cherokee"
+# define CHEROKEE_DOCDIR      "/usr/local/share/doc/cherokee"
+
+# define CHEROKEE_VAR_RUN     "/usr/local/var/run"
+# define CHEROKEE_FLCACHE     "/usr/local/var/lib/cherokee/flcache"
+# define CHEROKEE_RRD_DIR     "/usr/local/var/lib/cherokee/graphs"
+
+# define CHEROKEE_CONFDIR     "/usr/local/etc/cherokee"
+# define CHEROKEE_SERVER      "/usr/local/sbin/cherokee"
+# define CHEROKEE_WORKER      "/usr/local/sbin/cherokee-worker"
+# define CHEROKEE_ICONSDIR    "/usr/local/share/cherokee/icons"
+# define CHEROKEE_THEMEDIR    "/usr/local/share/cherokee/themes"
+# define CHEROKEE_PANIC_PATH  "/usr/local/bin/cherokee-panic"
+
+#endif
